93811026bc972f8a598ac00c90a3d65bef831b7f
berraciftlikk <80973708+berraciftlikk@users.noreply.github.com>
2022-03-24 15:56:12 +0300

    ASP.Net Core MVC

---
+++ BitirmeProjesi/CafeProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CafeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
